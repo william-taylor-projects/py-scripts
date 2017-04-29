44fa6d85162a33c666d83752b53b2645501a0795
william-taylor <wi11berto@yahoo.co.uk>
2017-04-29 16:33:51 +0100

    basic types + collections

--- learning/types.py
+++ learning/types.py
@@ -1,4 +1,6 @@
 
+# Data Types
+
 # <class 'float'>
 floatValue = 0.0
 
@@ -11,4 +13,20 @@ boolValue = False
 # <class 'str'>
 stringValue = "String"
 
-print(type(boolValue))
+# <class 'str' len = 1>
+charValue = stringValue[0]
+
+# Collection Types
+
+# <class 'tuple'>
+tupleValue = (0, 1, 2) # immutable
+
+# <class 'list'>
+listValues = [0, 1] # mutable
+list.append(listValues, 2)
+
+print(len(stringValue)) # 6
+print(len(listValues)) # 3
+print(len(tupleValue)) # 3
+
+# Logical Operations
