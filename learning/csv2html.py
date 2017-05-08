5574fbffa326d8fa06b7a7fea85b4a03acb19204
william-taylor <wi11berto@yahoo.co.uk>
2017-05-08 13:50:40 +0100

    adding chapter 2

--- learning/csv2html.py
+++ learning/csv2html.py
@@ -5,8 +5,59 @@ def print_start():
 def print_end():
     print("</table'>")
 
+def extract_fields(line):
+    fields = []
+    field = ""
+    quote = None
+    for c in line:
+        if c in "\"'":
+            if quote is None:
+                quote = c
+            elif quote == c:
+                quote = None
+            else:
+                field += c
+            continue
+        if quote is None and c == ",":
+            fields.append(field)
+            field = ""
+        else:
+            field += c
+    if field:
+        fields.append(field)
+    return fields
+
+def escape_html(text):
+    text = text.replace("&", "&amp;")
+    text = text.replace("<", "&lt;")
+    text = text.replace(">", "&gt;")
+    return text
+
 def print_line(line, color, maxwidth):
-    print("")
+    print("<tr bgcolor='{0}'>".format(color))
+    fields = extract_fields(line)
+    
+    for field in fields:
+        if not field:
+            print("<td></td>")
+        else:
+            number = field.replace(",", "")
+            
+            try:
+                x = float(number)
+                print("<td align='right'>{0:d}</td>".format(round(x)))
+           
+            except ValueError:
+                field = field.title()
+                field = field.replace(" And ", " and ")
+                
+                if len(field) <= maxwidth:
+                    field = escape_html(field)
+                else:
+                    field = "{0} ...".format(escape_html(field[:maxwidth]))
+                print("<td>{0}</td>".format(field))
+    print("</tr>")
+
 
 def main():
     maxwidth = 100
@@ -26,3 +77,5 @@ def main():
         except EOFError:
             break
     print_end()
+
+main()
