22ae089b6f588e9b20ba593382f611cfcadb1295
william-taylor <wi11berto@yahoo.co.uk>
2017-05-09 17:01:44 +0100

    basic grades script

--- scripts/grades/bsc-grades.py
+++ scripts/grades/bsc-grades.py
@@ -1,14 +1,63 @@
 
 import matplotlib.pyplot as plot
 
-xbar = [ 0, 1, 2 ]
-ybar = [ 85, 75, 90 ]
+year_one = {
+    "2D Graphics Programming": 81,
+    "Computing Systems": 60.03,
+    "Mathematics of Space & Change": 79,
+    "Introduction to Programming": 65,
+    "Intro to Games Development": 74,
+    "Creative Computing Profession": 61.5
+}
 
-fig = plot.figure(1)
-fig.canvas.set_window_title('University Grades')
+year_two = {
+    "Real Time 3D Graphics": 81.08,
+    "Interactive Physical Modelling": 68,
+    "Computing Project": 84,
+    "Structures and Algorithms": 75.8,
+    "Computer Games Design": 73,
+    "Game Engine Design": 82.2
+}
 
-plot.title('Grades')
-plot.plot(xbar, ybar)
-plot.xticks(xbar, ['A', 'B', 'C' ])
-plot.ylabel('Grade %')
-plot.show()
+year_three = {
+    "Computer Game AI": 85,
+    "Games Technology Project": 81.6,
+    "Mobile Games Development": 87.83,
+    "Games Project : Design and Plan": 71.4,
+    "Advanced Games Programming": 92.8,
+    "Algorithms and Collections": 79.2
+}
+
+year_four = {
+    "Games Console Development": 85.8,
+    "Serious Games": 81
+}
+
+def next_figure():
+  next_figure.counter += 1
+  return next_figure.counter
+next_figure.counter = 0
+
+def show_year(graph_title, year):
+    grades = list(year.values())
+    labels = list(range(len(grades)))
+    average = sum(grades) / len(grades)
+
+    plot.rcParams["figure.figsize"] = (12, 6)
+
+    fig = plot.figure(next_figure())
+    fig.canvas.set_window_title('University Grades')
+
+    plot.title(graph_title)
+    plot.plot(labels, list(year.values()))
+    plot.plot(labels, list(year.values()), 'gs')
+    plot.plot(list(range(-1, len(grades)+1)), [average] * (2+len(grades)))
+    plot.axis([-1, len(grades), 0, 100])
+    plot.xticks(labels, list(year.keys()), fontsize=5)
+    plot.ylabel('Grades %')
+    plot.show()
+
+show_year("Year One", year_one)
+show_year("Year Two", year_two)
+show_year("Year Three", year_three)
+show_year("Year Four", year_four)
