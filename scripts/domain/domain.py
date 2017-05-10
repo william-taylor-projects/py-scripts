c6cd148d77c2ec19922689ae2e1ed8c2ca8df6fa
william-taylor <wi11berto@yahoo.co.uk>
2017-05-10 12:45:17 +0100

    adding extra things to domain tester

--- scripts/domain/domain.py
+++ scripts/domain/domain.py
@@ -1,5 +1,18 @@
 # Domain Script:
 #   The following script should
 #      a) Test domains are active and healthy
+#      b) Print success error etc with color codes
+#      c) use cli parsing lib
 
-print 'Hello, World \n'
+from requests import get
+from colorama import init, Fore, Back, Style
+
+init()
+
+response = get("http://www.bing.com")
+
+print(response.status_code)
+
+print(Fore.GREEN + 'some red text')
+print(Fore.YELLOW + 'some red text')
+print(Fore.RED + 'some red text')
