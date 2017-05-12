dc0d992105d269c1cc5197b13cb410945d3b51b9
william-taylor <wi11berto@yahoo.co.uk>
2017-05-12 07:13:31 +0100

    basic implementation

--- scripts/hardware.py
+++ scripts/hardware.py
@@ -2,56 +2,58 @@
 Hardware descripter python program
 
 Usage:
-    
+    hardware.py [--cpu] [--gpu] [--ram] [--os]
+    hardware.py -h | --help | -v | --version
 
 Options:
-  
+    -c --cpu          Show CPU info
+    -g --gpu          Show GPU info
+    -r --ram          Show RAM info
+    -o --os           Show OS info
+    -h --help         Show this screen.
+    -v --version      Show version.
 
 Dependencies:
-   pip install python-hwinfo
+   pip install docopt
    pip install wmi
 
 Examples:
-    
+    hardware.py --gpu > gpu_info.txt
+    hardware.py --cpu
 """
 from docopt import docopt
 from wmi import WMI as WinInfo
 
-def get_cpu_info():
-    pass
+def get_cpu_info(pc, cpu_info):
+    print('CPU: {0}'.format(cpu_info.Name))
 
-def get_ram_info():
-    pass
+def get_ram_info(pc, os_info):
+    print("RAM: {0}".format(float(os_info.TotalVisibleMemorySize) / 1048576.0)) 
 
-def get_os_info():
-    pass
+def get_os_info(pc, os_info):
+    print('OS Name: {0}'.format(os_name))
+    print('OS Version: {0}'.format(os_version))
 
-def get_gpu_info():
-    pass
+def get_gpu_info(pc, gpu_info):
+    print('Graphics Card: {0}'.format(gpu_info.Name))
 
-computer = WinInfo()
+def main():
+    config = docopt(__doc__, version='0.1')
+    pc = WinInfo()
 
-computer_info = computer.Win32_ComputerSystem()[0]
-processor_info = computer.Win32_Processor()[0]
-gpu_info = computer.Win32_VideoController()[0]
-os_info = computer.Win32_OperatingSystem()[0]
+    processor_info = pc.Win32_Processor()[0]
+    gpu_info = pc.Win32_VideoController()[0]
+    os_info = pc.Win32_OperatingSystem()[0]
+    pc_info = pc.Win32_ComputerSystem()[0]
 
+    if config['--cpu']:
+        get_cpu_info(pc, processor_info)
+    if config['--gpu']:
+        get_gpu_info(pc, gpu_info)
+    if config['--ram']:
+        get_ram_info(pc, os_info)
+    if config['--os']:
+        get_os_info(pc, os_info)
 
-print(gpu_info)
-
-"""
-computer_info = computer.Win32_ComputerSystem()[0]
-os_info = computer.Win32_OperatingSystem()[0]
-proc_info = computer.Win32_Processor()[0]
-gpu_info = computer.Win32_VideoController()[0]
-
-os_name = os_info.Name
-os_version = ' '.join([os_info.Version, os_info.BuildNumber])
-system_ram = float(os_info.TotalVisibleMemorySize) / 1048576  # KB to GB
-
-print('OS Name: {0}'.format(os_name))
-print('OS Version: {0}'.format(os_version))
-print('CPU: {0}'.format(proc_info.Name))
-print('RAM: {0} GB'.format(system_ram))
-print('Graphics Card: {0}'.format(gpu_info.Name))
-"""
+if __name__ == '__main__':
+    main()
