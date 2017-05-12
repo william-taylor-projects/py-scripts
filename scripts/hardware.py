87c1ab631738e9de60db0d1fe9a11cae563e40c0
william-taylor <wi11berto@yahoo.co.uk>
2017-05-12 16:59:47 +0100

    basic info being printed

--- scripts/hardware.py
+++ scripts/hardware.py
@@ -26,16 +26,37 @@ from wmi import WMI as WinInfo
 
 def get_cpu_info(pc, cpu_info):
     print('CPU: {0}'.format(cpu_info.Name))
+    print('Threads: {0}'.format(cpu_info.ThreadCount))
+    print('Cores: {0}'.format(cpu_info.NumberOfCores))
+    print('Logical Processors: {0}'.format(cpu_info.NumberOfLogicalProcessors))
+    print('Current Clock Speed: {0}'.format(cpu_info.CurrentClockSpeed))
+    print('Max Clock Speed: {0}'.format(cpu_info.MaxClockSpeed))
+    print('Voltage: {0}'.format(cpu_info.CurrentVoltage))
+    print('L2 Cache: {0}'.format(cpu_info.L2CacheSize))
+    print('L3 Cache: {0}'.format(cpu_info.L3CacheSize))
+    print('Virtualization?: {0}'.format(cpu_info.VirtualizationFirmwareEnabled))
 
 def get_ram_info(pc, os_info):
-    print("RAM: {0}".format(float(os_info.TotalVisibleMemorySize) / 1048576.0)) 
+    print("Visiable Memory Size: {0}".format(os_info.TotalVisibleMemorySize))
+    print("Virtual Memory Size: {0}".format(os_info.TotalVirtualMemorySize)) 
+    print("Free Space In Paging Files: {0}".format(os_info.FreeSpaceInPagingFiles))
+    print("Free Physical Memory: {0}".format(os_info.FreePhysicalMemory)) 
+    print("Free Virtual Memory: {0}".format(os_info.FreeVirtualMemory)) 
 
 def get_os_info(pc, os_info):
-    print('OS Name: {0}'.format(os_name))
-    print('OS Version: {0}'.format(os_version))
+    print('OS Name: {0}'.format(os_info.Caption))
+    print('OS Version: {0}'.format(os_info.Version))
+    print('OS Architecture: {0}'.format(os_info.OSArchitecture))
+    print('Windows Dir: {0}'.format(os_info.WindowsDirectory))
+    print('Processes: {0}'.format(os_info.NumberOfProcesses))
+    print('Users: {0}'.format(os_info.NumberOfUsers))
 
 def get_gpu_info(pc, gpu_info):
     print('Graphics Card: {0}'.format(gpu_info.Name))
+    print('Driver Version: {0}'.format(gpu_info.DriverVersion))
+    print('Refresh Rate: {0}'.format(gpu_info.MaxRefreshRate))
+    print('Description: {0}'.format(gpu_info.VideoModeDescription))
+    print('Family: {0}'.format(gpu_info.VideoProcessor))
 
 def main():
     config = docopt(__doc__, version='0.1')
