523ff8c27e20a394634d2605091546d1b7fc63c5
william-taylor <wi11berto@yahoo.co.uk>
2017-05-31 00:34:32 +0100

    basic crypto script

---
+++ scripts/crypto.py
@@ -0,0 +1,24 @@
+"""
+Cryptography utility for securing data
+
+Usage:
+    crypto.py <text>
+
+"""
+from cryptography.fernet import Fernet
+from docopt import docopt
+
+def main():
+    config = docopt(__doc__, version='0.1')
+
+    key = Fernet.generate_key()
+    txt = bytes(config['<text>'], 'utf-8')
+
+    cipher_suite = Fernet(key)
+    cipher_text = cipher_suite.encrypt(txt)
+
+    print(key)
+    print(cipher_text)
+
+if __name__ == '__main__':
+    main()
